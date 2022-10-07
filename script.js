5a3b9b40f979a4ab69d6c8536302d89596fac378
Siddharth Satyakam <48103561+SiddharthaSatyakam@users.noreply.github.com>
2022-10-07 10:52:00 +0000

    Toggle the lights

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-document.getElementById("google").addEventListener("click",function() {
-  window.location = "https://www.google.co.in/";
-})
+// document.getElementById("google").addEventListener("click",function() {
+//   window.location = "https://www.google.co.in/";
+// })
 
-document.getElementById("fb").addEventListener("click",function() {
-  window.location = "https://www.facebook.com"
-})
+// document.getElementById("fb").addEventListener("click",function() {
+//   window.location = "https://www.facebook.com"
+// })
 
-document.getElementById("twitter").addEventListener("click",function() {
-  window.location = "https://twitter.com/";
-})
+// document.getElementById("twitter").addEventListener("click",function() {
+//   window.location = "https://twitter.com/";
+// })
+
+setInterval(function() {
+  document.querySelector("#bulb").classList.toggle("bulb")
+}, 300)
